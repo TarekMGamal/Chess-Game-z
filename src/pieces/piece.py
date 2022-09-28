7e9b902175ca16807e5e89c16bebbd3b00237890
Tarek Gamal <TarekMGamal1@gmail.com>
2022-09-28 22:40:29 +0200

    Add Piece class

---
+++ src/pieces/piece.py
@@ -0,0 +1,13 @@
+class Piece:
+    def __init__(self , x , y , color):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.alive = True
+
+    def move(self , new_x , new_y):
+        self.x = new_x
+        self.y = new_y
+
+    def kill_piece(self):
+        self.alive = False
